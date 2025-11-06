4e2b415949e97cf61ad15cd9f1168b510c4f30dd
Felix Gust <gustifix@users.noreply.github.com>
2025-11-06 11:05:26 +0100

    WIP transformer

--- cpp_kernels/transformer/src/host.cpp
+++ cpp_kernels/transformer/src/host.cpp
@@ -14,12 +14,11 @@
 * under the License.
 */
 #include "xcl2.hpp"
+#include <cstdlib>
 #include <vector>
 
-#define DATA_SIZE 2048
-#define INCR_VALUE 10
-// define internal buffer max size
-#define BURSTBUFFERSIZE 256
+constexpr int INPUT_SIZE = 16;
+constexpr int OUTPUT_SIZE = 32;
 
 int main(int argc, char** argv) {
     if (argc != 2) {
@@ -29,22 +28,20 @@ int main(int argc, char** argv) {
 
     std::string binaryFile = argv[1];
 
-    int size = DATA_SIZE;
-    int inc_value = INCR_VALUE;
     cl_int err;
     cl::Kernel krnl_add;
     cl::CommandQueue q;
     cl::Context context;
     // Allocate Memory in Host Memory
-    size_t vector_size_bytes = sizeof(int) * size;
-    std::vector<int, aligned_allocator<int> > source_inout(size);
-    std::vector<int, aligned_allocator<int> > source_sw_results(size);
+    // size_t vector_size_bytes = sizeof(int) * size;
+    // std::vector<int, aligned_allocator<int> > source_inout(size);
+    // std::vector<int, aligned_allocator<int> > source_sw_results(size);
 
-    // Create the test data and Software Result
-    for (int i = 0; i < size; i++) {
-        source_inout[i] = i;
-        source_sw_results[i] = i + inc_value;
-    }
+    // // Create the test data and Software Result
+    // for (int i = 0; i < size; i++) {
+    //     source_inout[i] = i;
+    //     source_sw_results[i] = i + inc_value;
+    // }
 
     // OPENCL HOST CODE AREA START
     auto devices = xcl::get_xil_devices();
@@ -65,7 +62,7 @@ int main(int argc, char** argv) {
             std::cout << "Failed to program device[" << i << "] with xclbin file!\n";
         } else {
             std::cout << "Device[" << i << "]: program successful!\n";
-            OCL_CHECK(err, krnl_add = cl::Kernel(program, "vadd", &err));
+            OCL_CHECK(err, krnl_add = cl::Kernel(program, "transformer", &err));
             valid_device = true;
             break; // we break because we found a valid device
         }
@@ -75,41 +72,43 @@ int main(int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
 
-    // Allocate Buffer in Global Memory
-    OCL_CHECK(err, cl::Buffer buffer_rw(context, CL_MEM_READ_WRITE | CL_MEM_USE_HOST_PTR, vector_size_bytes,
-                                        source_inout.data(), &err));
+    return EXIT_SUCCESS;
 
-    OCL_CHECK(err, err = krnl_add.setArg(0, buffer_rw));
-    OCL_CHECK(err, err = krnl_add.setArg(1, size));
-    OCL_CHECK(err, err = krnl_add.setArg(2, inc_value));
+    // // Allocate Buffer in Global Memory
+    // OCL_CHECK(err, cl::Buffer buffer_rw(context, CL_MEM_READ_WRITE | CL_MEM_USE_HOST_PTR, vector_size_bytes,
+    //                                     source_inout.data(), &err));
 
-    // Copy input data to device global memory
-    OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_rw}, 0 /* 0 means from host*/));
+    // OCL_CHECK(err, err = krnl_add.setArg(0, buffer_rw));
+    // OCL_CHECK(err, err = krnl_add.setArg(1, size));
+    // OCL_CHECK(err, err = krnl_add.setArg(2, inc_value));
 
-    // Launch the Kernel
-    OCL_CHECK(err, err = q.enqueueTask(krnl_add));
+    // // Copy input data to device global memory
+    // OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_rw}, 0 /* 0 means from host*/));
 
-    // Copy Result from Device Global Memory to Host Local Memory
-    OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_rw}, CL_MIGRATE_MEM_OBJECT_HOST));
-    OCL_CHECK(err, err = q.finish());
+    // // Launch the Kernel
+    // OCL_CHECK(err, err = q.enqueueTask(krnl_add));
 
-    // OPENCL HOST CODE AREA END
+    // // Copy Result from Device Global Memory to Host Local Memory
+    // OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_rw}, CL_MIGRATE_MEM_OBJECT_HOST));
+    // OCL_CHECK(err, err = q.finish());
 
-    // Compare the results of the Device to the simulation
-    int match = 0;
-    for (int i = 0; i < size; i++) {
-        if (source_inout[i] != source_sw_results[i]) {
-            std::cout << "Error: Result mismatch" << std::endl;
-            std::cout << "i = " << i << " CPU result = " << source_sw_results[i]
-                      << " Device result = " << source_inout[i] << std::endl;
-            match = 1;
-            break;
-        } else {
-            std::cout << source_inout[i] << " ";
-            if (((i + 1) % 16) == 0) std::cout << std::endl;
-        }
-    }
+    // // OPENCL HOST CODE AREA END
 
-    std::cout << "TEST " << (match ? "FAILED" : "PASSED") << std::endl;
-    return (match ? EXIT_FAILURE : EXIT_SUCCESS);
+    // // Compare the results of the Device to the simulation
+    // int match = 0;
+    // for (int i = 0; i < size; i++) {
+    //     if (source_inout[i] != source_sw_results[i]) {
+    //         std::cout << "Error: Result mismatch" << std::endl;
+    //         std::cout << "i = " << i << " CPU result = " << source_sw_results[i]
+    //                   << " Device result = " << source_inout[i] << std::endl;
+    //         match = 1;
+    //         break;
+    //     } else {
+    //         std::cout << source_inout[i] << " ";
+    //         if (((i + 1) % 16) == 0) std::cout << std::endl;
+    //     }
+    // }
+
+    // std::cout << "TEST " << (match ? "FAILED" : "PASSED") << std::endl;
+    // return (match ? EXIT_FAILURE : EXIT_SUCCESS);
 }
